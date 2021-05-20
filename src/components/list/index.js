b1726cb3f1c2225f782cef2f777c40ff2cff64db
sso <buffalo704@hotmail.com>
2021-05-20 08:32:51 -0400

    feat:autocomplete initial commit

---
+++ src/components/list/index.js
@@ -0,0 +1,81 @@
+import React, { useState, useEffect, useRef } from "react";
+
+const List = () => {
+  const [countries, setCountries] = useState([]);
+  const [filteredCountriesListItems, setFilteredCountriesListItems] = useState(
+    []
+  );
+  const [showList, setShowList] = useState(true);
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    fetch("http://localhost:8080/countries")
+      .then((response) => response.json())
+      .then((data) => setCountries(data.countries));
+  }, []);
+
+  const debounce = (func, timeout = 500) => {
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => {
+        func.apply(this, args);
+      }, timeout);
+    };
+  };
+
+  const processChange = debounce(() => renderList());
+
+  const handleSelect = (e) => {
+    setShowList(false);
+    inputEl.current.value = e.target.outerText;
+  };
+
+  const renderList = () => {
+    const filteredCountries =
+      inputEl.current.value &&
+      countries.filter((country) => {
+        return country.name
+          .toLowerCase()
+          .includes(inputEl.current.value.toLowerCase());
+      });
+
+    filteredCountries &&
+      setFilteredCountriesListItems(
+        filteredCountries.map((country) => (
+          <li
+            className="list-group-item"
+            key={country.code}
+            onClick={handleSelect}
+          >
+            {country.name}
+          </li>
+        ))
+      );
+    inputEl.current.value && setShowList(true);
+  };
+
+  return (
+    <form>
+      <div className="mb-3 control">
+        <label htmlFor="autocomplete" className="form-label">
+          AutoComplete
+        </label>{" "}
+        &nbsp;
+        <input
+          id="autocomplete"
+          ref={inputEl}
+          className="form-control input"
+          onKeyUp={processChange}
+        />
+      </div>
+      {showList && (
+        <div className="list">
+          <ul className="list-group">{filteredCountriesListItems}</ul>
+        </div>
+      )}
+    </form>
+  );
+};
+
+export default List;
